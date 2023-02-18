234e318bcbf3f92c77449f06072cc5c26ee6bb8e
louitron <luisgarcia1437@gmail.com>
2023-02-18 03:53:21 +0000

    Changed the program to accept .xls instead of .xml, tweaks to GUI and color scheme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# python_xml_compare_and_consolidate_pandas
-Program which takes 2 .xml files as input, compares them, and outputs a new .xml which only includes rows/columns with matching data. Created to automate a boring process in my day job.
+# python_xsl_compare_and_consolidate_pandas
+Program which takes 2 .xsl files as input, compares them, and outputs a new .xls which only includes rows/columns with matching data. Created to automate a boring process in my day job.
 
-using pandas, tkinker, and tkinker.filedialog to make it work with a user interface
+using pandas, tkinker to make it work with a user interface
--- main.py
+++ main.py
@@ -2,28 +2,28 @@ import tkinter as tk
 from tkinter import filedialog
 import pandas as pd
 
-def compare_xml_files(file1, file2):
-    # Read the first XML file into Pandas
-    df1 = pd.read_xml(file1)
+def compare_excel_files(file1, file2):
+    # Read the first Excel file into a Pandas dataframe
+    df1 = pd.read_excel(file1)
 
-    # Read the second XML file into Pandas
-    df2 = pd.read_xml(file2)
+    # Read the second Excel file into a Pandas dataframe
+    df2 = pd.read_excel(file2)
 
-    # Find the common columns between the two xml's
+    # Find the common columns between the two dataframes
     common_columns = list(set(df1.columns) & set(df2.columns))
 
-    # Filter the first xml to only include the common columns
+    # Filter the first excel to only include the common columns
     df1 = df1[common_columns]
 
-    # Filter the second xml to only include the common columns
+    # Filter the second excel to only include the common columns
     df2 = df2[common_columns]
 
-    # combine the two xmls on the common columns
+    # Merge the two excel on the common columns
     merged_df = pd.merge(df1, df2, on=common_columns, how='inner')
 
-    # Write the combined xml's to a new xml file
-    merged_df.to_xml('merged_data.xml', index=False)
-    status_label.config(text="Output file generated successfully.")
+    # Write the merged excel to a new Excel file
+    merged_df.to_excel('merged_data.xlsx', index=False)
+    status_label.config(text="Comparison and new file generated successfully.")
 
 def browse_file():
     file_path = filedialog.askopenfilename()
@@ -32,39 +32,40 @@ def browse_file():
 def run_comparison():
     file1 = file1_path.get()
     file2 = file2_path.get()
-    compare_xml_files(file1, file2)
+    compare_excel_files(file1, file2)
 
-# Cmain tk window
+# Create the main window
 root = tk.Tk()
-root.title("XML File Comparator")
+root.title("Excel File Comparison")
 root.geometry("600x300")
 root.config(bg="gray10")
 
-# file input and browse
-file1_label = tk.Label(root, text="Select File 1", fg="white", bg="gray10", font=("Arial", 12))
+# Create the file input fields and browse buttons
+file1_label = tk.Label(root, text="Select Workbook 1", fg="white", bg="gray10", font=("Arial", 12))
 file1_label.grid(row=0, column=0, padx=10, pady=10)
 file1_path = tk.StringVar()
 file1_path.set("")
 file1_entry = tk.Entry(root, textvariable=file1_path, width=50)
 file1_entry.grid(row=0, column=1, padx=10, pady=10)
-file1_button = tk.Button(root, text="Browse", command=lambda: file1_path.set(browse_file()), bg="gray")
-file1_button.grid(row=0, column=2, padx=10, pady=10)
+file1_button = tk.Button(root, text="Browse", command=lambda: file1_path.set(browse_file()), bg="olive")
+file1_button.grid(row=0, column=2, padx=10, pady=40)
 
-file2_label = tk.Label(root, text="Select File 2", fg="white", bg="gray10", font=("Arial", 12))
-file2_label.grid(row=1, column=0, padx=10, pady=10)
+file2_label = tk.Label(root, text="SelectWorkbook 2", fg="white", bg="gray10", font=("Arial", 12))
+file2_label.grid(row=1, column=0, padx=30, pady=10)
 file2_path = tk.StringVar()
 file2_path.set("")
 file2_entry = tk.Entry(root, textvariable=file2_path, width=50)
 file2_entry.grid(row=1, column=1, padx=10, pady=10)
-file2_button = tk.Button(root, text="Browse", command=lambda: file2_path.set(browse_file()), bg="gray")
+file2_button = tk.Button(root, text="Browse", command=lambda: file2_path.set(browse_file()), bg="olive")
 file2_button.grid(row=1, column=2, padx=10, pady=10)
 
-# run button
-run_button = tk.Button(root, text="Run Comparison", command=run_comparison, bg="gray")
+# Create the run button
+run_button = tk.Button(root, text="Run Comparison", command=run_comparison, bg="lime")
 run_button.grid(row=2, column=1, padx=10, pady=10)
 
-# status label
+# Create the status label
 status_label = tk.Label(root, text="", fg="white", bg="gray10", font=("Arial", 12))
 status_label.grid(row=3, column=1, padx=10, pady=10)
 
+# Run the main loop
 root.mainloop()
